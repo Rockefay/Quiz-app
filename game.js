c3597f9dc22c378deefbfb1ec7a12837aa54aaec
Krzysztof <kr.purgat@gmail.com>
2022-03-08 12:41:40 +0100

    Create a Spinning Loader

--- game.js
+++ game.js
@@ -3,6 +3,8 @@ const choices = Array.from(document.getElementsByClassName("choice-text"));
 const progressText = document.getElementById('progressText');
 const scoreText = document.getElementById('score');
 const progressBarFull = document.getElementById('progressBarFull');
+const loader = document.getElementById('loader');
+const game = document.getElementById('game');
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -41,6 +43,8 @@ startGame = () => {
     score = 0;
     availableQuesions = [...questions]; //spread operator to copy items from one array to another
     getNewQuestion();
+    game.classList.remove("hidden");
+    loader.classList.add("hidden");
 };
 
 getNewQuestion = () => {
